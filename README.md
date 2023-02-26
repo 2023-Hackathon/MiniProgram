965e4ee9fcb93ba3c3bab6f53b2697999d807132
HongruiTang <87180656+HongruiTang@users.noreply.github.com>
2023-02-26 07:41:53 +0000

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+## Description 
+
+This is a full stack project deployed on WeChat Mini Program using JavaScript. 
+- back-end: 
+  1. logic in processing heartrate, step number, and resistance data 
+  2. designing an AI algorithm for song chossing
+  3. sending the data to web platform which is desinged for health care workers
+- front-end: visualization of users' data and providing the ability of online music recommendations
+
+## How to deploy
+
+To deploy a WeChat Mini Program, you need to download WeChat Mini Program Developer tools.
+
+## LICENSE
+
+[MIT LICENSE](./LICENSE)
--- utils/btConnection.js
+++ utils/btConnection.js
@@ -1,21 +1,21 @@
 export class BtConnection {
   constructor(writeUUID="", notifyUUID="", serviceUUID="", deviceName="") {
-    this.serviceUUID = [] //主 service 的 uuid 列表
-    this.writeUUID = writeUUID.toUpperCase(); //写读 UUID
+    this.serviceUUID = [] 
+    this.writeUUID = writeUUID.toUpperCase(); 
     this.notifyUUID = notifyUUID.toUpperCase(); //notify UUID
-    this.filterServiceUUID = serviceUUID.toUpperCase(); //过滤获取到的服务uuid(有些会返回多条数据)
-    this.filterDeviceName = deviceName; //设备名称
+    this.filterServiceUUID = serviceUUID.toUpperCase(); 
+    this.filterDeviceName = deviceName; 
 
-    this.macAddress = ""; //保存得到mac地址
-    this.flagFromTypes = ''; //来源类型
+    this.macAddress = ""; 
+    this.flagFromTypes = ''; 
     this._discoveryStarted = false;
-    this.deviceId = ''; //用于区分设备的 id
+    this.deviceId = ''; 
 
     this._deviceId = '';
     this._serviceId = '';
     this._characteristicId = '';
-    this.status = false; //当前状态
-    this.action_type = 'NOTIFY'; //操作类型
+    this.status = false; 
+    this.action_type = 'NOTIFY'; 
     this.code = -1;
     this.isnotExist = true
 
@@ -66,7 +66,6 @@ export class BtConnection {
       },
       fail: (res) => {
         console.log('初始化蓝牙失败', res);
-        //自行处理【可弹窗提示用户开启蓝牙】，这通过回调处理
       }
     })
   }
@@ -77,13 +76,9 @@ export class BtConnection {
     wx.closeBluetoothAdapter()
   }
 
-  /**
- * 断开蓝牙连接
- */
+  
 closeBLEConnection() {
-	//停止搜索
     this.stopBluetoothDevicesDiscovery();
-    console.log("断开与低功耗蓝牙设备的连接。", this.deviceId);
 
     if (this.deviceId) {
         wx.closeBLEConnection({
@@ -100,7 +95,6 @@ closeBLEConnection() {
             }
         })
 
-        //关闭蓝牙模块
         wx.closeBluetoothAdapter({
             success: function(res) {
                 console.log("closeBluetoothAdapter ==>res:", res);
@@ -118,13 +112,9 @@ closeBLEConnection() {
 }
 
 
-  /**
-   * 监听寻找新设备事件
-   * 搜索匹配设备后，自动连接设备
-   */
+  
   onBluetoothDeviceFound() {
     wx.onBluetoothDeviceFound((res) => {
-      // console.log('广播数据结果:', res);
 
       res.devices.forEach(device => {
         if (!device.name && !device.localName) {
@@ -132,35 +122,25 @@ closeBLEConnection() {
         }
         console.log(device.name)
         if (device.name === this.filterDeviceName) console.log("FOUND !!!!!!!!!")
-        // 转换后, 得出相关数据
         var hexStr = this.ab2hex(device.advertisData);
-        // console.log("广播数据中转换后：advertisData---->" + hexStr);
-
-        //通过获取mac匹配
         if ((this.macAddress != "") && (this.macAddress == device.deviceId) && this.isnotExist) {
           this.isnotExist = false;
           this.deviceId = device.deviceId;
           console.log('android-->tempDeviceId:' + this.deviceId);
 
-          //停止搜寻附近的蓝牙外围设备
           this.stopBluetoothDevicesDiscovery();
 
-          // //连接设备
           this.createBLEConnection();
         }
 
-
-        //通过name匹配设备
         let deviceName = device.name;
         if ((deviceName.indexOf(this.filterDeviceName) != -1) && this.isnotExist) {
           this.isnotExist = false;
           this.deviceId = device.deviceId;
           console.log('ios or android-->tempDeviceId:' + this.deviceId);
 
-          //停止搜寻附近的蓝牙外围设备。
           this.stopBluetoothDevicesDiscovery();
 
-          //连接设备
           this.createBLEConnection();
         }
       })
@@ -168,18 +148,17 @@ closeBLEConnection() {
   }
 
   startBluetoothDevicesDiscovery() {
-    console.log("执行连接蓝牙设备 回调空===" + this._discoveryStarted);
     if (this._discoveryStarted) {
       return;
     }
     this._discoveryStarted = true
 
     wx.startBluetoothDevicesDiscovery({
-      services: this.serviceUUID, //如果设置此参数，则只搜索广播包有对应 uuid 的主服务的蓝牙设备。
+      services: this.serviceUUID, 
       allowDuplicatesKey: false,
       success: (res) => {
         console.log('启动搜索蓝牙设备, 结果  :', res)
-        this.onBluetoothDeviceFound() //先调用此方法再使startBluetoothDevicesDiscovery
+        this.onBluetoothDeviceFound() 
       },
       fail(res) {
         console.log('startBluetoothDevicesDiscovery fail', res);
@@ -187,16 +166,13 @@ closeBLEConnection() {
     })
   }
 
-  //停止搜寻附近的蓝牙外围设备。
   stopBluetoothDevicesDiscovery() {
     console.log('end discovery')
     this._discoveryStarted = false
     wx.stopBluetoothDevicesDiscovery()
   }
 
-  /**
-   * 连接蓝牙设备
-   */
+
   createBLEConnection() {
     console.log("creating BLE connection")
     var that = this;
@@ -215,28 +191,23 @@ closeBLEConnection() {
         // asddErrorCallback(res.errCode, "");
       }
     })
-    //停止搜索
     this.stopBluetoothDevicesDiscovery();
   }
 
   getBLEDeviceServices(deviceId) {
-    //监听低功耗蓝牙连接状态的改变事件
     wx.onBLEConnectionStateChange(function (res) {
       console.log("onBLEConnectionStateChange:", res);
-      // 该方法回调中可以用于处理连接意外断开等异常情况
       console.log(`device ${res.deviceId} state has changed, connected: ${res.connected}`)
       if (res.connected == false) {
         console.log("连接意外断开等****", this._deviceId);
         this.isnotExist = true
         this._deviceId = '';
         if (this.flagFromTypes == 1 && this.flagFromTypes == 2) {
-          // asddErrorCallback(1010, "");
           console.log("??getBLEDeviceServices??")
         }
       }
     });
 
-    //获取蓝牙所有service
     wx.getBLEDeviceServices({
       deviceId: deviceId,
       success: (res) => {
@@ -245,7 +216,6 @@ closeBLEConnection() {
           let tmpUuid = res.services[i].uuid.toUpperCase();
           console.log(`finding desired service: ${this.filterServiceUUID.toUpperCase()}   checking: ${tmpUuid}`)
           if ((res.services[i].isPrimary) && (tmpUuid.indexOf(this.filterServiceUUID.toUpperCase()) != -1)) {
-            //获取蓝牙特征值
             console.log('service found')
             this.getBLEDeviceCharacteristics(deviceId, res.services[i].uuid)
             return
@@ -259,24 +229,18 @@ closeBLEConnection() {
     })
   }
 
-  /**
-   * 获取蓝牙特征值
-   */
   getBLEDeviceCharacteristics(deviceId, serviceId) {
     let that = this
-    console.log("设备：" + deviceId + '******************服务：' + serviceId);
     wx.getBLEDeviceCharacteristics({
       deviceId: deviceId,
       serviceId: serviceId,
       success: (res) => {
-        // console.log('蓝牙设备特征值信息:', res);
         console.log('getBLEDeviceCharacteristics success', res.characteristics)
         for (let i = 0; i < res.characteristics.length; i++) {
           let item = res.characteristics[i]
-          var itemUUID = item.uuid.toUpperCase(); //转大写
+          var itemUUID = item.uuid.toUpperCase(); 
           console.log(`checking item: ${itemUUID}`)
           console.log(that.notifyUUID)
-          //read操作
           if (item.properties.read && itemUUID == that.writeUUID) {
             wx.readBLECharacteristicValue({
               deviceId: deviceId,
@@ -285,37 +249,26 @@ closeBLEConnection() {
             })
           }
 
-          //write操作
           if (item.properties.write && itemUUID == that.writeUUID) {
-            console.log("写 特征值 -----------------------" + item.uuid);
             that._deviceId = deviceId
             that._serviceId = serviceId
             that._characteristicId = item.uuid
 
-
-            console.log("发送 信息查询指令 【根据需求捏】")
-            //发送 信息查询指令 【根据需求】
-            if (that.flagFromTypes === 'e') { //血压、秤
-              console.log("发送 信息查询指令 【根据需求】")
+            if (that.flagFromTypes === 'e') { 
               that.status = true
               that.writeData(0x31)
-            }
-            
+            }   
           }
 
-
-          //notify操作，注意调用监听特征值变化
           if (that.notifyUUID == itemUUID) {
             console.log('notify service start processing')
             if (item.properties.notify || item.properties.indicate) {
-              console.log('调用notifyBLECharacteristicValueChange前', item.uuid);
               wx.notifyBLECharacteristicValueChange({
                 deviceId: deviceId,
                 serviceId: serviceId,
                 characteristicId: item.uuid,
                 state: true,
                 success(res) {
-                  console.log('notification通知数据', res);
                   that.status = true;
                   // wx.hideLoading();
                 },
@@ -333,20 +286,14 @@ closeBLEConnection() {
       }
     })
 
-
-    // 操作之前先监听，保证第一时间获取数据
     wx.onBLECharacteristicValueChange(function (res) {
       console.log(`characteristic ${res.characteristicId} has changed, now is ${res.value}`)
 
-      console.log("操作类型:" + that.action_type);
-
-      console.log("设备原始数据--->", res.value)
-      var resData = that.ab2hex(res.value); //转16进制
+      var resData = that.ab2hex(res.value); 
 
       console.log("设备返回数据--->", resData); 
       that.data = resData
       
-      // 判断不同类型处理数据
       switch(that.flagFromTypes){
         case "h":
           console.log('writing heartrate data')
@@ -361,9 +308,6 @@ closeBLEConnection() {
     })
   }
 
-  /**
-   * 写入数据
-   */
   writeData(hex, action = '') {
     
     if (!this.status) {
@@ -382,13 +326,10 @@ closeBLEConnection() {
       console.log(99999980908908070789878696758645)
       console.log(hex)
 
-      //类型转换
       var enDataBuf = new Uint8Array([hex]);
       console.log(enDataBuf)
       var buffer1 = enDataBuf.buffer
       console.log(buffer1)
-      console.log("发送内容长度：", buffer1.byteLength)
-      console.log('写入的数据：' + this._deviceId + '服务serviceId---》' + this._serviceId + '特征characteristicId---》' + this._characteristicId);
 
       wx.writeBLECharacteristicValue({
         deviceId: this._deviceId,
@@ -397,50 +338,33 @@ closeBLEConnection() {
         value: buffer1,
         success: (res) => {
           wx.hideLoading();
-          console.log("写数据返回结果", res.errMsg);
 
-          //项目需求： 发送某个指令后的需要处理回调
           if (action == 'lastZero') {
-            console.log('最后一次写入00需执行回调========》');
-            //回调 目的: 执行调用提交接口
-            // eyeCareObj.eyeCareCallback();
+            console.log('========');
           }
         },
         fail(res) {
-          console.log("写数据失败..", res);
+          console.log(res);
           // asddErrorCallback(res.errCode, "");
         }
       })
     }, 1000)
   }
 
-
-
-  /**
-   *  匹配规则: 取名称后面的mac地址
-   *  mac地址: 假设C7:E2:90:17:1A:40
-   * 	len: 截取长度为
-   */
   getNameMac(macAddress, len, name) {
     let clearColonMac = clearSymbol(macAddress);
     let lastFourMac = clearColonMac.substring(clearColonMac.length - len);
     let strName = name.toUpperCase();
-    strName = strName + lastFourMac.toUpperCase(); //转大写
+    strName = strName + lastFourMac.toUpperCase(); 
     console.log('拼接后的' + strName); //abc_171A40
     return strName
   }
 
-  /**
-   * 去掉 冒号
-   */
   clearSymbol(str) {
-    str = str.replace(/:/g, ""); //取消字符串中出现的所有冒号
+    str = str.replace(/:/g, ""); 
     return str;
   }
 
-  /**
-   * rrayBuffer转16进度字符串
-   */
   ab2hex(buffer) {
     var hexArr = Array.prototype.map.call(
       new Uint8Array(buffer),
